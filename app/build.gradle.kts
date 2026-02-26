1cc5c5dffa782376652636fd1889d46d843768c8
GOMPANGE <rlwls1223@icloud.com>
2026-02-26 10:57:42 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "tenrikyo.ofudesaki.kr"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "9.0"
+        versionCode = 10
+        versionName = "10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
